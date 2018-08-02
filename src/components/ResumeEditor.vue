c648576bd1b30d7c542cf23dc0ff1c1e8163a30e
unknown <1694623679@qq.com>
2018-08-02 14:46:12 +0800

    tab 切换

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -2,22 +2,31 @@
     <div id="resumeEditor">
        <nav>
            <ol>
-               <li class="active">x</li>
+               <!-- <li class="active">x</li>
                <li>x</li>
                <li>x</li>
                <li>x</li>
                <li>x</li>
                <li>x</li>
-               <li>x</li>
+               <li>x</li> -->
+               <li v-for="(item,index) in resume.visibleItems"
+                   :class="{active:item === selected}"
+                   @click="selected = item"
+                   >
+                     {{index}}
+               </li>
            </ol>
            <ol class="panels">
-                <li>1</li>
+                <!-- <li>1</li>
                 <li>2</li>
                 <li>3</li>
                 <li>4</li>
                 <li>5</li>
                 <li>6</li>
-                <li>7</li>
+                <li>7</li> -->
+                <li v-for="item in resume.visibleItems" v-show="item===selected">
+                    {{resume[item]}}
+                </li>
            </ol>
        </nav>
     </div>
@@ -27,7 +36,21 @@
         name:'ResumeEditor',
         data(){
             return{
-
+               selected:'bio',
+               resume:{
+                   visibleItems:['bio','work history','education','projects','awards','contacts','others'],
+                   bio:{
+                       name:'',
+                       city:'',
+                       title:''
+                   },
+                   'work history':[],
+                    education:[],
+                    projects:[],
+                    awards:[],
+                    contacts:[],
+                    others:[]
+               }
             }
         }
     }
@@ -35,11 +58,11 @@
 </script>
 <style lang="scss">
     #resumeEditor{
-       background: #ffffff;
+       background: white;
        box-shadow: 0 2px 3px rgba(0,0,0,0.6);
        display: flex;
        flex-direction: row;
-    
+       overflow: auto;
     >nav{
         width:80px;
         background:black;
@@ -48,7 +71,7 @@
             >li{
                 height:48px;
                 display:flex;
-                justify-content:content;
+                justify-content:center;
                 align-items:center;
                 margin-top: 16px;
                 margin-bottom: 16px;
