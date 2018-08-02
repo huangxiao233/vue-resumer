675a841247b1c8f995b2f35abf4de21b83395a11
unknown <1694623679@qq.com>
2018-08-02 14:01:43 +0800

    add buttons

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,10 @@
    <div id="topbar">
        <div class="wrapper">
            <span class="logo">Resumer</span>
+           <div class="actions">
+               <button class="primary">保存</button>
+               <button>预览</button>
+           </div>
        </div>
    </div>
 </template>
@@ -10,27 +14,42 @@
         name:'Topbar'
     }
 </script>
-<style>
+<style scoped lang="scss">
     #topbar{
         background: #ffffff;
         box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
-        height:64px;
-    }
-    #topbar>.wrapper{
-        min-width: 1024px;
+    >.wrapper{
+        min-width:1024px;
         max-width:1440px;
         margin: 0 auto;
         height:64px;
     }
-    #topbar>.wrapper{
+    >.wrapper{
         display: flex;
         justify-content: space-between;
         align-items:center;
         padding: 0 16px;
     }
-    #topbar .logo{
+    .logo{
         font-size:20px;
         color:#000000;
         
     }
+}
+button{
+   width:72px;
+   height:32px;
+   font-size:18px;
+   color:#222;
+   border:none;
+   background: #ddd;
+   cursor:pointer;
+   &:hover{
+       box-shadow: 1px 1px 1px hsla(0,0,0,0.5);
+   }
+  &.primary{
+       background: #02af5f;
+       color:white;
+    }
+  }
 </style>
