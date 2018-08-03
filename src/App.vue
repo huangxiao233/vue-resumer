7ff02ac8038eed9f93df4b834091e2ab915cfc00
unknown <1694623679@qq.com>
2018-08-03 15:50:58 +0800

    使用setter让数据可以写

--- src/App.vue
+++ src/App.vue
@@ -21,13 +21,10 @@ import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
 import icons from './assets/icons'
+import store from './store/index'
 export default {
   name: 'app',
-  data:function(){
-    return{
-      text:'hello'
-    }
-  },
+  store,
   components:{Topbar,ResumeEditor,ResumePreview},
   created(){
     document.body.insertAdjacentHTML('afterbegin',icons)
